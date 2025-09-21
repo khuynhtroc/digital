e7fd5ce310f76ab202eb06eb6d66aa27f15e601a
Darius <huynhnt135@gmail.com>
2025-09-22 00:11:27 +0700

    Darius: update

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      <p>Trang bạn tìm không tồn tại.</p>
+    </div>
+  );
+}
